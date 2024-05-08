7c07a625bb1138dcc17f20a68feccded064ac763
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-08 14:53:28 +0300

    Lecture Materials

---
+++ Lecture-2 06.07.2024/DataMining/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace DataMining;
+
+public class Customer
+{
+    //CustomerKey,Gender,Name,City,State Code,State,Zip Code,Country,Continent,Birthday
+    public int CustomerKey { get; set; }
+    public string Gender { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string StateCode { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Country { get; set; }
+    public string Continent { get; set; }
+    public DateTime Birthday { get; set; }
+}
