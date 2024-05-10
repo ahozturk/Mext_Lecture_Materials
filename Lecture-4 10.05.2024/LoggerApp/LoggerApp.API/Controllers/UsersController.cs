28e44a4a45a42da6db865f53d0160c4252d3aca4
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-10 16:04:29 +0300

    Lecture Materials

---
+++ Lecture-4 10.05.2024/LoggerApp/LoggerApp.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using LoggerApp.Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggerApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly LoggerApp.Domain.Abstractions.ILogger _logger;
+
+        public UsersController(LoggerApp.Domain.Abstractions.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _logger.Log("All Users Sent");
+            return Ok("Users");
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            _logger.Log("User Added");
+            return Ok();
+        }
+    }
+}
---
+++ Lecture-4 10.05.2024/LoggerApp/LoggerApp.Domain/Services/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using LoggerApp.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerApp.Domain.Services
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
