9f91a9720c0868bf1e3da9b2943b1a174bdefca4
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-13 11:45:47 +0300

    Lecture Materials

---
+++ Lecture-5 13.05.2024/EventListApp/EventListApp.Domain/Entities/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace EventListApp.Domain;
+
+public class Event
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTimeOffset Date { get; set; }
+    public Person Owner { get; set; }
+}
---
+++ Lecture-5 13.05.2024/EventListApp/EventListApp.Domain/Entities/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace EventListApp.Domain;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+}
