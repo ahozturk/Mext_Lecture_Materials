eb044e61defeb6d41f881e183581f98b9c2fda3d
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-15 16:04:26 +0300

    Lecture Materials

---
+++ Lecture-7 15.05.2024/GPTExample/GPTExample.Persistence/Contexts/GPTExampleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GPTExample.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace GPTExample.Persistence;
+
+public class GPTExampleDbContext : DbContext
+{
+    public DbSet<GPTPrompt> Prompts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=GPTExampleDb;User Id=postgres;Password=mysecretpassword;");
+    }
+}
--- Lecture-7 15.05.2024/GPTExample/GPTExample.Persistence/Services/PromptService.cs
+++ Lecture-7 15.05.2024/GPTExample/GPTExample.Persistence/Services/PromptService.cs
@@ -1,12 +1,27 @@
 ﻿using GPTExample.Application;
+using GPTExample.Domain;
 
 namespace GPTExample.Persistence;
 
 public class PromptService : IPromptService
 {
+    private readonly GPTExampleDbContext _context;
+
+    public PromptService(GPTExampleDbContext context)
+    {
+        _context = context;
+    }
+
     public void Add(AddPromptDto addPromptDto)
     {
-        throw new NotImplementedException();
+        _context.Prompts.Add(new GPTPrompt
+        {
+            PromptText = addPromptDto.PromptText,
+            CreatedBy = addPromptDto.CreatedBy,
+            // CreatedDate = DateTimeOffset.UtcNow
+        });
+
+        _context.SaveChanges();
     }
 
     public List<GetPromptDto> GetPrompts(string userId)
