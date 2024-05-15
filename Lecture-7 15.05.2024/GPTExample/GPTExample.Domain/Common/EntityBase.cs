1867b3500ce1881507f16752e57a154400c3a091
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-15 15:31:25 +0300

    Lecture Materials

---
+++ Lecture-7 15.05.2024/GPTExample/GPTExample.Domain/Common/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace GPTExample.Domain;
+
+public class EntityBase<TKey>
+{
+    public TKey Id { get; set; }
+
+    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset ModifiedDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ Lecture-7 15.05.2024/GPTExample/GPTExample.Domain/Entities/GPTPrompt.cs
@@ -0,0 +1,7 @@
+﻿namespace GPTExample.Domain;
+
+public class GPTPrompt : EntityBase<Guid>
+{
+    public string PromptText { get; set; }
+    public string ResponseText { get; set; }
+}
