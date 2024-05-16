10a940c132ba850e07f1a81f3defd60617449b95
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-16 16:03:54 +0300

    Lecture Materials

--- Lecture-9 16.05.2024/OnionArchitectureTemplate/Template.Infrastructure/Services/NotepadService.cs
+++ Lecture-9 16.05.2024/OnionArchitectureTemplate/Template.Infrastructure/Services/NotepadService.cs
@@ -1,6 +1,13 @@
-﻿namespace Template.Infrastructure;
+﻿using Template.Application;
 
-public class NotepadService
+namespace Template.Infrastructure;
+
+public class NotepadService : INotepadService
 {
+    public void WriteToTextFile(string text)
+    {
+        var path = "/Users/hakan/Documents/GitHub/Mext_Lecture_Materials/Lecture-9 16.05.2024/OnionArchitectureTemplate/Template.Presentation/notepad.txt";
 
+        File.AppendAllText(path, text);
+    }
 }
