dcf162b4f9f3e2ebf7724586dc2838a3a00aa1ab
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-16 16:09:46 +0300

    Lecture Materials

---
+++ Lecture-9 16.05.2024/OnionArchitectureTemplate/Template.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Template.Domain;
+
+namespace Template.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=TemplateAppDb;User Id=postgres;Password=mysecretpassword;");
+    }
+}
--- Lecture-9 16.05.2024/OnionArchitectureTemplate/Template.Persistence/Services/ProductService.cs
+++ Lecture-9 16.05.2024/OnionArchitectureTemplate/Template.Persistence/Services/ProductService.cs
@@ -1,6 +1,23 @@
-﻿namespace Template.Persistence;
+﻿using Template.Application;
 
-public class ProductService
+namespace Template.Persistence;
+
+public class ProductService : IProductService
 {
+    private readonly AppDbContext _context;
 
+    public ProductService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(AddProductDto addProductDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<GetProductDto> GetAll(Paginition paginition)
+    {
+        throw new NotImplementedException();
+    }
 }
