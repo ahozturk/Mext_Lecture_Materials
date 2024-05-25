bce0147d780e4b0cea5abbeaa44a6dd158ff31c9
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-25 09:51:08 +0300

    Lecture Materials

--- Lecture-12 20.05.2024/Productify/Productify.Application/Dtos/UpdateProductDto.cs
+++ Lecture-12 20.05.2024/Productify/Productify.Application/Dtos/UpdateProductDto.cs
@@ -5,4 +5,6 @@ public class UpdateProductDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string description { get; set; }
+
 }
--- Lecture-12 20.05.2024/Productify/Productify.Application/Services/MappingService.cs
+++ Lecture-12 20.05.2024/Productify/Productify.Application/Services/MappingService.cs
@@ -6,6 +6,9 @@ public static class MappingService
 {
     public static GetProductDto ConvertToGetProductDto(Product product)
     {
+        if (product is null)
+            return null;
+
         return new GetProductDto
         {
             Id = product.Id,
@@ -17,6 +20,9 @@ public static class MappingService
 
     public static Product ConvertToProduct(GetProductDto addProductDto)
     {
+        if (addProductDto is null)
+            return null;
+
         return new Product
         {
             Id = addProductDto.Id,
