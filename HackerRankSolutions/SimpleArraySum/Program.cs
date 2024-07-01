36cf6aeb7918c6d242f237292ef3d09305e2e187
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-01 11:23:49 +0300

    Lecture Materials

---
+++ HackerRankSolutions/SimpleArraySum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int simpleArraySum(List<int> ar)
+    {
+        int result = 0;
+        
+        foreach (int element in ar)
+        {
+            result += element;
+        }
+        
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
