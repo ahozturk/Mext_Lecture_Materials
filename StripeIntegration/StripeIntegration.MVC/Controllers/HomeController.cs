e951ab2b98303561457c83c56e79513631b98740
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-01 16:06:14 +0300

    Lecture Materials

--- StripeIntegration/StripeIntegration.MVC/Controllers/HomeController.cs
+++ StripeIntegration/StripeIntegration.MVC/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
     [HttpPost]
         public ActionResult GoPayment(string productId)
         {
-            var domain = "http://localhost:4242";
+            var domain = "http://localhost:5062";
             var options = new SessionCreateOptions
             {
                 LineItems = new List<SessionLineItemOptions>
@@ -31,13 +31,13 @@ public class HomeController : Controller
                   new SessionLineItemOptions
                   {
                     // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    Price = productId,
+                    Price = "price_1PXjTXBuMoTa3aiJyDwA3Th4",
                     Quantity = 1,
                   },
                 },
                 Mode = "payment",
-                SuccessUrl = domain + "/success.html",
-                CancelUrl = domain + "/cancel.html",
+                SuccessUrl = domain + "/home/success",
+                CancelUrl = domain + "/home/cancel",
             };
             var service = new SessionService();
             Session session = service.Create(options);
@@ -45,4 +45,14 @@ public class HomeController : Controller
             Response.Headers.Add("Location", session.Url);
             return new StatusCodeResult(303);
         }
+
+    public IActionResult Success()
+    {
+        return View();
+    }
+
+    public IActionResult Cancel()
+    {
+        return View();
+    }
 }
